594a0ed3d852f6126ee37e313a24539c3e858374
Zhenyan Zhu <lukezhuz@umich.edu>
2024-05-04 10:45:19 -0400

    Fixed sample input

--- test/lower_to_llvm.sh
+++ test/lower_to_llvm.sh
@@ -1,3 +1,2 @@
-polygeist-opt test.mlir --memory-access-generation --mlir-disable-threading -o test_memacc.mlir
-polygeist-opt test_memacc.mlir --lower-affine -o test_memacc.mlir
-polygeist-opt test_memacc.mlir --convert-polygeist-to-llvm -o test_memacc.mlir
+polygeist-opt test_memacc.mlir --lower-affine -o test_memacc.mlir.scf
+polygeist-opt test_memacc.mlir.scf --convert-polygeist-to-llvm -o test_memacc.mlir.llvm
--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,5 @@
-void gather(double *a, double *b, int *idx, int n, const int C) {
-  static double dest[100];
+void gather(double *a, double *b, int *idx, int n) {
   for (int i = 0; i < n; i++) {
     a[i] = b[idx[i]];
-    dest[i] = a[i] + C;
   }
 }
