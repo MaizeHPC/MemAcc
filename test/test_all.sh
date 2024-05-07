cd26241e9ba57a5cfd528bcf7c3392e873754542
Zhenyan Zhu <lukezhuz@mbit10.eecs.umich.edu>
2024-05-07 13:40:46 -0400

    Cleaned test_all logs

--- test/test_all.sh
+++ test/test_all.sh
@@ -1,13 +1,13 @@
 export PATH=$PATH:/home/lukezhuz/MemAcc/build/bin/
 
 echo "[LOG]: converting test.cpp to test.mlir"
-cgeist test.cpp -raise-scf-to-affine -O0 -S -o test.mlir -I/usr/include/x86_64-linux-gnu -I/usr/lib/gcc/x86_64-linux-gnu/11/include -I/usr/include/linux/
+cgeist test.cpp -raise-scf-to-affine -O0 -S -o test.mlir -I/usr/include/x86_64-linux-gnu -I/usr/lib/gcc/x86_64-linux-gnu/11/include -I/usr/include/linux/ &> /dev/null
 
 echo "[LOG]: generating memory access information"
-bash test_memacc.sh
+bash test_memacc.sh &> /dev/null
 
 echo "[LOG]: lowering to LLVM"
-bash lower_to_llvm.sh
+bash lower_to_llvm.sh &> /dev/null
 
 echo "[LOG]: running test"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
