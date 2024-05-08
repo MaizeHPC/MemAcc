ed8589b716428b95da933d5fc73044ecb51f7999
Zhenyan Zhu <lukezhuz@mbit10.eecs.umich.edu>
2024-05-08 17:50:39 -0400

    Added final demo for May8 Presentation

---
+++ test/demo_may_8/scripts/clean.sh
@@ -0,0 +1,3 @@
+rm test_memacc.*
+rm test_memacc
+rm test.mlir
--- test/demo_may_8/test_all.sh
+++ test/demo_may_8/test_all.sh
@@ -4,26 +4,26 @@ echo "[LOG]: Converting C/C++ to MLIR using Polygeist"
 cgeist test.cpp -raise-scf-to-affine -O0 -S -o test.mlir -I/usr/include/x86_64-linux-gnu -I/usr/lib/gcc/x86_64-linux-gnu/11/include -I/usr/include/linux/ &> /dev/null
 
 echo "[LOG]: Capturing indirect memory accesses and hoisting them out of loop (hardware-agnostic)"
-bash test_memacc.sh &> /dev/null
+bash scripts/memacc_gen.sh &> /dev/null
 if [ $? -ne 0 ]; then
     echo "[ERROR]: failed to generate memory access information"
     exit 1
 fi
 
 echo "[LOG]: Lowering to generic memory access to target API call (hardware-specific)"
-bash lower_to_llvm.sh &> /dev/null
+bash scripts/lower_to_llvm.sh &> /dev/null
 if [ $? -ne 0 ]; then
     echo "[ERROR]: failed to lower to LLVM"
     exit 1
 fi
 
 echo "[LOG]: Compiling to executable and link target library"
-bash code_gen.sh &> /dev/null
+bash scripts/code_gen.sh &> /dev/null
 if [ $? -ne 0 ]; then
     echo "[ERROR]: failed to compile to executable"
     exit 1
 fi
 
 echo "[LOG]: running test"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../MAA
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../MAA
 ./test_memacc
